c18057379b0564eb4bec42f31ba82436f264802c
mixxx73 <m.macierzynski@gmail.com>
2025-04-17 02:42:15 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+extern crate chrono;
+extern crate env_logger;
+extern crate iron;
+extern crate logger;
+extern crate router;
+extern crate rustc_serialize;
+extern crate uuid;
+
+
+mod database;
+mod handlers;
+mod models;
+
+use models::*;
+use database::Database;
+use handlers::*;
+
+use iron::prelude::Chain;
+use iron::Iron;
+use router::Router;
+use logger::Logger;
+use uuid::Uuid;
+
+fn main() {
+    env_logger::init();
+    let (logger_before, logger_after) = Logger::new(None);
+
+    let mut db = Database::new();
+    let p = Post::new(
+        "post okok 1",
+        "body 1",
+        "Jon none",
+        chrono::offset::utc::UTC::now(),
+        Uuid::new_v4(),
+    );
+    db.add_post(p);
+
+    let p2 = Post::new(
+        "post 2",
+        "body 2",
+        "Jon gone",
+        chrono::offset::utc::UTC::now(),
+        Uuid::new_v4(),
+    );
+    db.add_post(p2);
+
+    let handlers = Handlers::new(db);
+    let json_content_middleware = JsonAftweMiddleware;
+
+    let mut router = Router::new();
+    router.get("/post_feed", handlers.post_feed, "post_feed");
+    router.post("/post", handlers.post_post, "post_post");
+    router.get("/post/:id", handlers.post, "post");
+
+    let mut chain = Chain::new(router);
+    chain.link_before(logger_before);
+    chain.link_after(json_content_middleware);
+    chain.link_after(logger_after);
+
+
+    Iron::new(chain).http("localhost:8000").unwrap();
+}
---
+++ src/models.rs
@@ -0,0 +1,37 @@
+use chrono::datetime::DateTime;
+// use chrono::offset::utc::UTC;
+use chrono::Utc;
+use uuid::Uuid;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Post {
+    title: String,
+    body: String,
+    author: String,
+    datetime: DateTime<UTC>,
+    uuid: Uuid,
+}
+
+impl Post {
+
+    pub fn new(
+        title: &str,
+        body: &str,
+        author: &str,
+        datetime: DateTime<UTC>,
+        uuid: Uuid,
+    ) -> Post {
+        Post {
+            title: title.to_string(),
+            body: body.to_string(),
+            author: author.to_string(),
+            datetime,
+            uuid,
+        }
+    }
+
+    pub fn uuid(&self) -> &Uuid {
+        &self.uuid
+    }
+}
